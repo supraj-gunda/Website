040ee9c0781c1e7fae783e68062a236c63216d48
supraj-gunda <67659221+supraj-gunda@users.noreply.github.com>
2025-02-21 15:17:00 -0500

    Delete growPlant.js

--- growPlant.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let randomNumber = Math.floor(Math.random() * 8) + 1;
-    let imagePath = `flower${randomNumber}.gif`;
-
-    document.getElementById("flowerImage").src = imagePath;
-});
